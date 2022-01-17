8286cbe15eebfc1117ed3372c9861959bd3de782
m04kA <60301384+m04kA@users.noreply.github.com>
2022-01-17 14:04:17 +0300

    Final
    This is final.

--- app.py
+++ app.py
@@ -1,72 +1,93 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from mongoLib import *
 import pandas as pd
 from fuzzywuzzy import process
 
+app = Flask(__name__, template_folder='templates')
 
-# app = Flask(__name__)
-# Сброс ограничений на количество выводимых рядов
+# Сброс ограничений на количество выводимых рядов (отладка)
 pd.set_option('display.max_rows', None)
 
-# Сброс ограничений на число столбцов
+# Сброс ограничений на число столбцов (отладка)
 pd.set_option('display.max_columns', None)
 
-# Сброс ограничений на количество символов в записи
+# Сброс ограничений на количество символов в записи (отладка)
 pd.set_option('display.max_colwidth', None)
+
+# Отключаем перенос строки для вывода (отладка)
+pd.options.display.expand_frame_repr = False
 
 
 def conecting_to_DB(name_DB, name_collection, port=27017):
+    """
+    Connect to DB
+    :param name_DB:
+    :param name_collection:
+    :param port:
+    :return:
+    """
     client = pymongo.MongoClient(port=port).DBNAME
     DB = client[name_DB]
     collection_name = DB[name_collection]
     return collection_name
 
 
-def find_in_documents(words):
+def find_in_documents(words, limits=20):
+    """
+    This funk take all documents and looking for matches. If there are no exact matches,
+    then it looks for partial matches.
+    :param words:
+    :return:
+    """
     collection_name = conecting_to_DB("Search_queries", "All_data")
     all_doks = pd.DataFrame(find_document(collection_name, all_documents=True))
+    all_doks.set_index('id', inplace=True)
     del all_doks['_id']
-    help_find_df = all_doks[['text', 'id']]
-    help_find_df.set_index('id', inplace=True)
-    all_doks_result = process.extract(words, all_doks['text'], limit=18)
+    all_doks_result = process.extract(words, all_doks['text'], limit=limits)
     mass_index = []
     for el in all_doks_result:
-        mass_index.append(el[0])
-    answer = all_doks.loc[all_doks['text'].isin(mass_index)]
+        mass_index.append(el[2])
 
-    print(answer)
+    answer = all_doks.loc[mass_index]
+    return answer
 
 
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
+@app.route('/', methods=['post', 'get'])
+def search_queries():
+    """
+    This funk need for render template and get data from user.
+    :return:
+    """
+    message = pd.DataFrame()
+    if request.method == 'POST':
+        user_text = request.form.get('users_text')
+        message = find_in_documents(user_text)
+    return render_template('find.html', tables=[message.to_html(classes='data', header="true")])
 
 
 if __name__ == '__main__':
-    find_in_documents('е')
-    # app.run()
-
     # ---- Update DB ----
-    # df = pd.read_csv('posts.csv', sep=',')
-    # df['created_date'] = pd.to_datetime(df['created_date'], yearfirst=True)
-    # df = df.sort_values(by='created_date')
-    # collection_name = conecting_to_DB("Search_queries", "All_data")
-    # for ind, row in df.iterrows():
-    #     data_row = {
-    #         "id": ind,
-    #         "rubrics": row["rubrics"],
-    #         "text": row["text"],
-    #         "created_date": row["created_date"]
-    #     }
-    #     result = find_document(collection_name, {"id": ind})
-    #     if result:
-    #         data_row = result | data_row
-    #         update_document(collection_name, {'id': ind}, data_row)
-    #     else:
-    #         collection_name.create_index([("id", pymongo.ASCENDING)], unique=True)
-    #         collection_name.create_index([("text", 'text')])
-    #         insert_document(collection_name, data_row)
+    df = pd.read_csv('posts.csv', sep=',')
+    df['created_date'] = pd.to_datetime(df['created_date'], yearfirst=True)
+    df = df.sort_values(by='created_date')
+    collection_name = conecting_to_DB("Search_queries", "All_data")
+    for ind, row in df.iterrows():
+        data_row = {
+            "id": ind,
+            "rubrics": row["rubrics"],
+            "text": row["text"],
+            "created_date": row["created_date"]
+        }
+        result = find_document(collection_name, {"id": ind})
+        if result:
+            data_row = result | data_row
+            update_document(collection_name, {'id': ind}, data_row)
+        else:
+            collection_name.create_index([("id", pymongo.ASCENDING)], unique=True)
+            collection_name.create_index([("text", 'text')])
+            insert_document(collection_name, data_row)
     # -----------------------
-    # rez = collection_name.find({"$text": {"$search": tt}})
-    # for el in rez:
-    #     print(el)
+
+    # ---- Start ----
+    app.run(debug=True)
+    # ---------------
